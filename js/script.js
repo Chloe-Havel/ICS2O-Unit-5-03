db03efdd3b7b6e6eefe95d854d1cafb2810bd0b1
Chloe Havel <113055595+Chloe-Havel@users.noreply.github.com>
2022-11-22 14:17:58 -0500

    stuff

--- js/script.js
+++ js/script.js
@@ -12,27 +12,13 @@ if (navigator.serviceWorker) {
     scope: "/ICS20-Unit-5-02/",
   })
 }
+
 /**
  * This function updates the slider value.
  */
 function updateSliderValue(valueFromSlider) {
   document.getElementById("slider-value").innerHTML = valueFromSlider
-}
+  //process
+let age = valueFromSlider
 
-/**
- * This function displays the slider value.
- */
-function myButtonClicked() {
-  buttonOnChecked = document.getElementById("flash1").checked
-
-  if (buttonOnChecked == true) {
-    const randomNumber = Math.floor(Math.random() * 6) + 1
-    document.getElementById("radio-button-value").innerHTML =
-      "<p>Value is: " + randomNumber + " </p>"
-  } else {
-    let randomNumber = Math.floor(Math.random() * 6) + 1
-    randomNumber = randomNumber * -1
-    document.getElementById("radio-button-value").innerHTML =
-      "<p>Value is: " + randomNumber + " </p>"
-  }
-}
+if age ==
